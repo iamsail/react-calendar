3e7a576adc56b71ac95c1647c1b661e3cf3a2a69
Sail <865605793@qq.com>
2019-12-07 19:11:28 +0800

    feat: add webpack

---
+++ build/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+      app: path.join(__dirname, './..', 'src/index.tsx')
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].js'
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+        // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+        { test: /\.tsx?$/, loader: "ts-loader" }
+    ]
+  }
+};
